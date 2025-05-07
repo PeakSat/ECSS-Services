db9b5a45acc033519fb88c0d5fbcfba0f76dc2d6
ktsoupos <konstantinostsoupos@gmail.com>
2025-05-07 15:15:44 +0300

    raise event in  both message and no message case

--- src/ErrorHandler.cpp
+++ src/ErrorHandler.cpp
@@ -55,7 +55,9 @@ void ErrorHandler::reportError(const Message& message, InternalErrorType errorCo
 #ifdef SERVICE_REQUESTVERIFICATION
 	Services.requestVerification.failStartExecutionVerification(message, getSpacecraftErrorCodeFromECSSError(errorCode));
 #endif
-
+	String<ECSSEventDataAuxiliaryMaxSize> eventMessage("");
+	eventMessage.append(static_cast<uint8_t>(errorCode), sizeof(uint8_t));
+	PMON_Handlers::raiseEvent(EventReportService::FailedStartOfExecution, EventType::InternalErrorType, EventReportService::LowSeverityAnomalyReport, eventMessage);
 	logError(message, errorCode);
 }
 
